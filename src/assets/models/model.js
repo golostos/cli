2b985f296b244e3e5c0d3f8009d4b3e31260ac45
Alexander Dmitriev <alex.golostos@gmail.com>
2019-03-27 02:42:12 +0400

    Replace legacy model creation by class representation

--- src/assets/models/model.js
+++ src/assets/models/model.js
@@ -1,18 +1,25 @@
 'use strict';
 
-module.exports = (sequelize, DataTypes) => {
-  const <%= name %> = sequelize.define('<%= name %>', {
-    <% attributes.forEach(function(attribute, index) { %>
-      <%= attribute.fieldName %>: DataTypes.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(DataTypes.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
-      <%= (Object.keys(attributes).length - 1) > index ? ',' : '' %>
-    <% }) %>
-  }, {
-    <%= underscored ? 'underscored: true,' : '' %>
-  });
+const { Model } = require('sequelize');
+<%= '\n' %>
+class <%= name %> extends Model {
+  static init(sequelize, DataTypes) {
+    return super.init({
+      <% attributes.forEach(function(attribute, index) { %>
+        <%= attribute.fieldName %>: DataTypes.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(DataTypes.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
+        <%= (Object.keys(attributes).length - 1) > index ? ',' : '' %>
+      <% }) %>
+      },
+      {
+        sequelize,
+        <%= underscored ? 'underscored: true,' : '' %>
+      }
+    )
+  }
 
-  <%= name %>.associate = function(models) {
+  static associate(models) {
     // associations can be defined here
-  };
+  }
+}
 
-  return <%= name %>;
-};
+module.exports = <%= name %>;
